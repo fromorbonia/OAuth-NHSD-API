7a6fcedc8ac2da90e5947ac92844f4f2f6b8ca0c
FromOrbonia <Aubyn@ASLAN-VI>
2020-07-23 22:16:49 +0100

    Add project files.

---
+++ src/oauth-nhsd-api/Pages/ProtectedPage.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace oauth_nhsd_api.Pages
+{
+    [Authorize]
+    public class ProtectedPageModel : PageModel
+    {
+        public string ResResponse { get; set; }
+
+        public string ResContent { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public ProtectedPageModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task OnGet()
+        {
+            var user = HttpContext.User;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+
+            var authenticationInfo = await HttpContext.AuthenticateAsync();
+
+            HttpRequestMessage req = new HttpRequestMessage(System.Net.Http.HttpMethod.Get,
+                _configuration["NHSD:APIEndpoint"] + "/Patient/9000000009");
+
+            req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            HttpResponseMessage response = await new HttpClient().SendAsync(req);
+
+            ResResponse = response.StatusCode.ToString();
+            ResContent = response.ToString();
+        }
+    }
+}
---
+++ src/oauth-nhsd-api/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+
+namespace oauth_nhsd_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                    options.DefaultChallengeScheme = "NHSD";
+                })
+                .AddCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SameSite = SameSiteMode.Lax;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                })
+                .AddOAuth("NHSD", options =>
+                {
+                    options.AuthorizationEndpoint = Configuration["NHSD:OAuthEndpoint"] + "/authorize";
+                    options.CallbackPath = new PathString("/NHSDBrokerModelModelCallBack2");
+
+                    options.ClientId = Configuration["NHSD:ClientId"];
+                    options.ClientSecret = Configuration["NHSD:ClientSecret"];
+                    options.TokenEndpoint = Configuration["NHSD:OAuthEndpoint"] + "/token";
+                    options.SaveTokens = true;
+                });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
